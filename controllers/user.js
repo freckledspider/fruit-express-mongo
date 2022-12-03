487d5feeaddddec7c91828db20d541fa0d9dacc8
bri <bcamesi@gmail.com>
2022-12-03 14:55:53 -0500

    user login and sign up buttons an pages

--- controllers/user.js
+++ controllers/user.js
@@ -19,8 +19,16 @@ router.get("/signup", (req, res) => {
     res.render("user/signup.ejs")
 })
 
-router.post("/signup", (req, res) => {
-    res.send("signup")
+// router.post("/signup", (req, res) => {
+//     res.send("signup")
+// })
+
+router.post("/signup", async (req, res) => {
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+
+    User.create(req.body, (err, user) => {
+        res.redirect("/user/login")
+    })
 })
 
 // The login Routes (Get => form, post => submit form)
@@ -29,8 +37,25 @@ router.get("/login", (req, res) => {
 })
 
 router.post("/login", (req, res) => {
-    res.send("login")
-})
+    const { username, password } = req.body;
+    //can also say req.body.username where we see username
+    //same w/ password. we can say req.body.password
+
+    User.findOne({ username }, (err, user) => {
+        if (!user) {
+            res.send("user doesnt exist");
+        } else {
+            const result = bcrypt.compareSync(password, user.password);
+            if (result) {
+                res.redirect("/fruits");
+            } else {
+
+                res.render("user/login.ejs", {data: 'wrong pass'});
+                // res.send("wrong password");
+            }
+        }
+    });
+});
 
 //////////////////////////////////////////
 // Export the Router
